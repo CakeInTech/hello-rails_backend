63b5c509177987efddbe6bc47a9484761a4f53e9
MohamedCK <abdulhakimm280@gmail.com>
2023-03-02 22:20:53 +0200

    - Update the rack cors to whitelist frontend

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::GreetingsController < ApplicationController
-  before_action :set_greeting, only: %i[ show update destroy ]
+  before_action :set_greeting, only: %i[show update destroy]
 
   # GET /greetings
   def index
